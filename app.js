f14f01fbf74f00e1c158af9af6b0922c027fdaff
Anil Kumar <mallula9010@gmail.com>
2021-06-25 19:38:49 +0530

    make it better

--- app.js
+++ app.js
@@ -4,30 +4,30 @@ const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 app.use(cookieParser());
 app.use(express.json());
-
-PORT = process.env.PORT || 5000;
-const path = require('path')
+const path = require("path");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/mernAuth",
+  "mongodb://localhost:27017/mernAuth",
   { useUnifiedTopology: true, useNewUrlParser: true },
   () => {
     console.log("Successfully connected to database");
   }
 );
 
-//serve tsatic assets in production
-if (process.env.NODE_ENV === "production") {
-  //set static folder
-  app.use(express.static("client/build"));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build','index.html');
-  })
-}
-//
 const userRouter = require("./routes/User");
 app.use("/user", userRouter);
 
-app.listen(PORT, () => {
-  console.log("Express server started on port 5000");
+const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
